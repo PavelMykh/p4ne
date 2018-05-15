dd353f583bd431bf46b24091c902c470996aef0a
PavelMykh <PanzerGit2018!!!>
2018-05-15 18:51:08 +0300

    comm-2

--- Lab1.2/plot.py
+++ Lab1.2/plot.py
@@ -12,9 +12,9 @@ def getV(x):    return x.value
 A_V = list(map(getV,sheet_data['A'][1:]))
 B_V = list(map(getV,sheet_data['B'][1:]))
 C_V = list(map(getV,sheet_data['C'][1:]))
+D_V = list(map(getV,sheet_data['D'][1:]))
 print(A_V)
 
-grap1 = pyplot.plot(A_V, B_V)
-grap2 = pyplot.plot(A_V, C_V)
+pyplot.plot(A_V, C_V)
+pyplot.plot(A_V, D_V)
 pyplot.show(grap1)
-pyplot.show(grap2)
